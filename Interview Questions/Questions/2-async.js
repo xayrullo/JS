55c355fd92bf982574cf8fc49115f60626c92978
xayrullo <xayrullo.jaloldinov@gmail.com>
2022-09-07 11:26:40 +0500

    JS questions -> second question added

---
+++ Interview Questions/Questions/2-async.js
@@ -0,0 +1,19 @@
+let num = 0;
+async function increment() {
+    num += await 2;
+    console.log(num);
+}
+increment();
+num += 1;
+console.log(num);
+
+/* 
+    * What is the resulting output?
+    * Qanday natija chiqadi?
+    * Каков результат?
+
+    * 2, 3
+    * 1, 3
+    * 1, 2
+    * 2, 1
+*/
