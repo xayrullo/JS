209d6fa421462db21ef1e969bcf528ce70ca369e
xayrullo <xayrullo.jaloldinov@gmail.com>
2022-11-14 14:23:38 +0500

    my concat added

---
+++ Methods/Array/concat.js
@@ -0,0 +1,11 @@
+function myConcat(...props) {
+  let concated = [...this];
+  for (let i = 0; i < props.length; i++) {
+    concated = [...concated, ...props[i]];
+  }
+  return concated;
+}
+
+Array.prototype.myConcat = myConcat;
+
+console.log([0].concat(obj1, obj2));
