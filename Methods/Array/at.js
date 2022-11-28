aa79f6ebe4621bc2a50fd3360ccfba1516220ac7
xayrullo <xayrullo.jaloldinov@gmail.com>
2022-11-28 16:38:00 +0500

    myAt property added

---
+++ Methods/Array/at.js
@@ -0,0 +1,11 @@
+function myAt(param) {
+  if (Math.abs(param) > this.length) return undefined;
+  if (param >= 0) return this[param];
+  return this[this.length + param];
+}
+
+Array.prototype.myAt = myAt;
+
+const arr = [1, 2, 3, 4, 5];
+
+console.log(arr.myAt(5));
